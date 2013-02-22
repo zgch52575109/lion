a59b68c27c0220631fe6c368cf3163ce51311683
zgch52575109 <zgch0512@163.com>
2013-02-21 23:55:38 -0800

    首次修改登录注册

--- lion168/index.php
+++ lion168/index.php
@@ -4,7 +4,6 @@ $cacheid = "index";
 $t->assign('page_name',$cacheid);
 
 $t->assign('noticear',get_notice_list('1'));
-
 if(GetCookie('EKuid')!=''){
 	$t->assign('loginname',GetCookie('EKUserName'));
 	$t->assign('loginuid',GetCookie('EKuid'));
